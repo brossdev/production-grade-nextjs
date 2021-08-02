e1ff5a3e8df0d5ab203632c53891a3983b8bc5e0
Bobby Ross <bobby.dev@pm.me>
2021-08-02 20:16:05 +0100

    feat: add auth setup for github

--- pages/app/[[...id]].tsx
+++ pages/app/[[...id]].tsx
@@ -8,6 +8,7 @@ import User from '../../components/user'
 import FolderPane from '../../components/folderPane'
 import DocPane from '../../components/docPane'
 import NewFolderDialog from '../../components/newFolderDialog'
+import { getSession , useSession } from 'next-auth/client'
 
 const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs?: any[] }> = ({
   folders,
@@ -15,9 +16,14 @@ const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs
   activeFolder,
   activeDocs,
 }) => {
-  const router = useRouter()
+    const router = useRouter()
+    const [session, loading] = useSession()
   const [newFolderIsShown, setIsShown] = useState(false)
-
+    
+    if (loading) {
+        return null
+   
+   }
   const Page = () => {
     if (activeDoc) {
       return <DocPane folder={activeFolder} doc={activeDoc} />
@@ -30,7 +36,7 @@ const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs
     return null
   }
 
-  if (false) {
+  if (!loading && !session) {
     return (
       <Dialog
         isShown
@@ -56,11 +62,11 @@ const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs
           <NewFolderButton onClick={() => setIsShown(true)} />
         </Pane>
         <Pane>
-          <FolderList folders={folders} />{' '}
+      <FolderList folders={[{ _id: 1, name: 'hello' }]} />{' '}
         </Pane>
       </Pane>
       <Pane marginLeft={300} width="calc(100vw - 300px)" height="100vh" overflowY="auto" position="relative">
-        <User user={{}} />
+        <User user={session.user} />
         <Page />
       </Pane>
       <NewFolderDialog close={() => setIsShown(false)} isShown={newFolderIsShown} onNewFolder={() => {}} />
@@ -72,6 +78,14 @@ App.defaultProps = {
   folders: [],
 }
 
+
+export async function getServerSideProps(ctx) {
+    const session = await getSession(ctx)
+
+    return {
+        props: {session},
+    }
+}
 /**
  * Catch all handler. Must handle all different page
  * states.
